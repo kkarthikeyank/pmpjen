e1c3b9c9a94f7508905872280a00942c4ef2a18e
kkarthikeyank <141196159+kkarthikeyank@users.noreply.github.com>
2025-07-29 10:53:27 +0530

    Update example.spec.js admin portal

--- src/tests/example.spec.js
+++ src/tests/example.spec.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
 
 // test.only('has title', async ({ page }) => {
 //   await page.goto('https://letcode.in/');
@@ -8,13 +8,34 @@ import { test, expect } from '@playwright/test';
 //   await expect(page).toHaveTitle( 'LetCode with Koushik' );
 // });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-console.log('Test completed successfully.');
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// console.log('Test completed successfully.');
+});
+
+// @ts-check
+import { test, expect } from '@playwright/test';
+test.use({ headless: true });
+test.only('has title', async ({ page }) => {
+  await page.goto('https://hikepmp-dev.smilecdr.com/admin/member-portal/#/login');
+  
+    await page.waitForTimeout(5000); // waits for 2 seconds
+await page.locator('//button[@id="loginButton"]').click();
+  console.log('Login button clicked');
+  await page.waitForTimeout(10000);
+  await page.locator('//input[@id="lookupFirstName"]').click();
+
+   await page.locator('//input[@id="lookupFirstName"]').fill('julia');
+
+   await page.locator("//button[normalize-space()='Search']").click();
+  console.log('Search button clicked');
+
+  // Expect a title "to contain" a substring.
+  // await expect(page).toHaveTitle( 'LetCode with Koushik' );
 });
