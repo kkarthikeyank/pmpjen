b4564e9c88f8472d8714449a5dbb7762f8c34616
kkarthikeyank <141196159+kkarthikeyank@users.noreply.github.com>
2025-07-29 11:00:34 +0530

    Update example.spec.js ad portal

--- src/tests/example.spec.js
+++ src/tests/example.spec.js
@@ -1,23 +1,23 @@
-// @ts-check
-// import { test, expect } from '@playwright/test';
+// // @ts-check
+// // import { test, expect } from '@playwright/test';
 
-// test.only('has title', async ({ page }) => {
-//   await page.goto('https://letcode.in/');
+// // test.only('has title', async ({ page }) => {
+// //   await page.goto('https://letcode.in/');
 
-//   // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle( 'LetCode with Koushik' );
+// //   // Expect a title "to contain" a substring.
+// //   await expect(page).toHaveTitle( 'LetCode with Koushik' );
+// // });
+
+// // test('get started link', async ({ page }) => {
+// //   await page.goto('https://playwright.dev/');
+
+// //   // Click the get started link.
+// //   await page.getByRole('link', { name: 'Get started' }).click();
+
+// //   // Expects page to have a heading with the name of Installation.
+// //   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// // console.log('Test completed successfully.');
 // });
-
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// console.log('Test completed successfully.');
-});
 
 // @ts-check
 import { test, expect } from '@playwright/test';
